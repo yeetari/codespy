2cc5c39d6050f6ff99446ff545ee86a565e3b3a0
Owen Smith <yeeetari@gmail.com>
2023-05-04 16:29:14 +0100

    bytecode: Comment out visit function parameter names to silence warning

--- include/codespy/bytecode/Visitor.hh
+++ include/codespy/bytecode/Visitor.hh
@@ -19,29 +19,33 @@ struct ClassVisitor {
 };
 
 struct CodeVisitor {
-    virtual void visit_constant(Constant constant) {}
-    virtual void visit_load(BaseType type, std::uint8_t local_index) {}
-    virtual void visit_store(BaseType type, std::uint8_t local_index) {}
-    virtual void visit_array_load(BaseType type) {}
-    virtual void visit_array_store(BaseType type) {}
-    virtual void visit_cast(BaseType from_type, BaseType to_type) {}
-    virtual void visit_compare(BaseType type, bool greater_on_nan) {}
-    virtual void visit_new(StringView descriptor, std::uint8_t dimensions = 1) {}
-    virtual void visit_get_field(StringView owner, StringView name, StringView descriptor, bool instance) {}
-    virtual void visit_put_field(StringView owner, StringView name, StringView descriptor, bool instance) {}
-    virtual void visit_invoke(InvokeKind kind, StringView owner, StringView name, StringView descriptor) {}
-    virtual void visit_math_op(BaseType type, MathOp math_op) {}
-    virtual void visit_monitor_op(MonitorOp monitor_op) {}
-    virtual void visit_reference_op(ReferenceOp reference_op) {}
-    virtual void visit_stack_op(StackOp stack_op) {}
-    virtual void visit_type_op(TypeOp type_op, StringView descriptor) {}
-    virtual void visit_iinc(std::uint8_t local_index, std::int32_t increment) {}
-    virtual void visit_goto(std::int32_t offset) {}
-    virtual void visit_if_compare(CompareOp compare_op, std::int32_t true_offset, CompareRhs compare_rhs) {}
-    virtual void visit_table_switch(std::int32_t low, std::int32_t high, std::int32_t default_pc,
-                                    Span<std::int32_t> table) {}
-    virtual void visit_lookup_switch(std::int32_t default_pc, Span<std::pair<std::int32_t, std::int32_t>> table) {}
-    virtual void visit_return(BaseType type) {}
+    virtual void visit_constant(Constant /*constant*/) {}
+    virtual void visit_load(BaseType /*type*/, std::uint8_t /*local_index*/) {}
+    virtual void visit_store(BaseType /*type*/, std::uint8_t /*local_index*/) {}
+    virtual void visit_array_load(BaseType /*type*/) {}
+    virtual void visit_array_store(BaseType /*type*/) {}
+    virtual void visit_cast(BaseType /*from_type*/, BaseType /*to_type*/) {}
+    virtual void visit_compare(BaseType /*type*/, bool /*greater_on_nan*/) {}
+    virtual void visit_new(StringView /*descriptor*/, std::uint8_t /*dimensions*/ = 1) {}
+    virtual void visit_get_field(StringView /*owner*/, StringView /*name*/, StringView /*descriptor*/,
+                                 bool /*instance*/) {}
+    virtual void visit_put_field(StringView /*owner*/, StringView /*name*/, StringView /*descriptor*/,
+                                 bool /*instance*/) {}
+    virtual void visit_invoke(InvokeKind /*kind*/, StringView /*owner*/, StringView /*name*/,
+                              StringView /*descriptor*/) {}
+    virtual void visit_math_op(BaseType /*type*/, MathOp /*math_op*/) {}
+    virtual void visit_monitor_op(MonitorOp /*monitor_op*/) {}
+    virtual void visit_reference_op(ReferenceOp /*reference_op*/) {}
+    virtual void visit_stack_op(StackOp /*stack_op*/) {}
+    virtual void visit_type_op(TypeOp /*type_op*/, StringView /*descriptor*/) {}
+    virtual void visit_iinc(std::uint8_t /*local_index*/, std::int32_t /*increment*/) {}
+    virtual void visit_goto(std::int32_t /*offset*/) {}
+    virtual void visit_if_compare(CompareOp /*compare_op*/, std::int32_t /*true_offset*/, CompareRhs /*compare_rhs*/) {}
+    virtual void visit_table_switch(std::int32_t /*low*/, std::int32_t /*high*/, std::int32_t /*default_pc*/,
+                                    Span<std::int32_t> /*table*/) {}
+    virtual void visit_lookup_switch(std::int32_t /*default_pc*/,
+                                     Span<std::pair<std::int32_t, std::int32_t>> /*table*/) {}
+    virtual void visit_return(BaseType /*type*/) {}
 };
 
 } // namespace codespy::bc
