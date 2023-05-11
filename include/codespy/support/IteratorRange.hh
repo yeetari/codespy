99666320690971369377d642399900c488b00898
Owen Smith <yeeetari@gmail.com>
2023-05-11 22:14:02 +0100

    support: Add MutableIterator and adapt_mutable_range()

--- include/codespy/support/IteratorRange.hh
+++ include/codespy/support/IteratorRange.hh
@@ -15,8 +15,24 @@ public:
 };
 
 template <typename It>
+class MutableIterator {
+    It m_it;
+
+public:
+    MutableIterator(It it) : m_it(it) {}
+    MutableIterator &operator++() { return *this; }
+    decltype(auto) operator*() { return *m_it++; }
+    bool operator==(const MutableIterator &other) const { return m_it == other.m_it; }
+};
+
+template <typename It>
 auto make_range(It begin, It end) {
     return IteratorRange(begin, end);
+}
+
+template <typename Range>
+auto adapt_mutable_range(Range &&range) {
+    return make_range(MutableIterator(range.begin()), MutableIterator(range.end()));
 }
 
 } // namespace codespy
